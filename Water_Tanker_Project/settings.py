4908238bd30d7fa798193c53d2d6925fb2791342
ShahnawazKhan423 <shahnawazkhan96850@gmail.com>
2025-12-15 17:52:20 +0530

    Update Dockerfile

--- Water_Tanker_Project/settings.py
+++ Water_Tanker_Project/settings.py
@@ -100,12 +100,15 @@ WSGI_APPLICATION = 'Water_Tanker_Project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': config('DB_ENGINE'),
-        'NAME': config("DB_NAME"),
-        'USER': config("DB_USER"),
-        'PASSWORD': config("DB_PASSWORD"),
-        'HOST': config("DB_HOST", default="localhost"),
-        'PORT': config("DB_PORT", default="5432"),
+        'ENGINE': config(
+            'DB_ENGINE',
+            default='django.db.backends.postgresql'
+        ),
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST', default='localhost'),
+        'PORT': config('DB_PORT', default=5432, cast=int),
     }
 }
 # Password validation
